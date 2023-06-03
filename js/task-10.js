f64f4740228d6ec316661497c62787ee5ec00259
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-06-03 05:17:25 +0300

    change-task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,8 +1,7 @@
-const elementControls = document.querySelector("#controls");
-const onInputNumber = elementControls.firstElementChild;
+const onInputNumber = document.querySelector("input");
 const buttonCreate = document.querySelector("button[data-create]");
 const buttonDestroy = document.querySelector("button[data-destroy]");
-const boxesElements = elementControls.nextElementSibling;
+const boxesElements = document.querySelector("#boxes");
 buttonCreate.addEventListener("click", onClickCountBoxesElements);
 buttonDestroy.addEventListener("click", onClickDeleteBoxesElements);
 function onClickCountBoxesElements() {
@@ -11,15 +10,13 @@ function onClickCountBoxesElements() {
 function createBoxes(amount) {
 	let divTotalArray = [];
 	for (let i = 1; i <= amount; i = i + 1) {
-		const markupDiv = `<div class = "box"></div>`;
+		let step = i * 10;
+		const markupDiv = `<div class = "box" style = "width: ${30 + step}px; height: ${
+			30 + step
+		}px; background-color: ${getRandomHexColor()}"></div>`;
 		divTotalArray.push(markupDiv);
 	}
 	boxesElements.insertAdjacentHTML("beforeend", divTotalArray.join(""));
-	const elementBox = document.querySelectorAll(".box");
-	elementBox.forEach((box, index) => {
-		index *= 10;
-		box.style.cssText = `width: ${30 + index}px; height: ${30 + index}px; background-color: ${getRandomHexColor()}`;
-	});
 }
 function onClickDeleteBoxesElements(event) {
 	onInputNumber.value = "";
@@ -31,7 +28,7 @@ function getRandomHexColor() {
 		.padStart(6, 0)}`;
 }
 
-// Варіант 2 стукання до силекторів
+// Варіант 2 бородатий на милицях  - стукання до силекторів плюс звернення і передавання стилів через forEach()
 // const elementControls = document.querySelector("#controls");
 // const onInputNumber = elementControls.firstElementChild;
 // const buttonCreate = onInputNumber.nextElementSibling;
@@ -39,3 +36,28 @@ function getRandomHexColor() {
 // const boxesElements = elementControls.nextElementSibling;
 // buttonCreate.addEventListener("click", onClickCountBoxesElements);
 // buttonDestroy.addEventListener("click", onClickDeleteBoxesElements);
+// function onClickCountBoxesElements() {
+// 	createBoxes(onInputNumber.value);
+// }
+// function createBoxes(amount) {
+// 	let divTotalArray = [];
+// 	for (let i = 1; i <= amount; i = i + 1) {
+// 		const markupDiv = `<div class = "box"></div>`;
+// 		divTotalArray.push(markupDiv);
+// 	}
+// 	boxesElements.insertAdjacentHTML("beforeend", divTotalArray.join(""));
+// 	const elementBox = document.querySelectorAll(".box");
+// 	elementBox.forEach((box, index) => {
+// 		index *= 10;
+// 		box.style.cssText = `width: ${30 + index}px; height: ${30 + index}px; background-color: ${getRandomHexColor()}`;
+// 	});
+// }
+// function onClickDeleteBoxesElements(event) {
+// 	onInputNumber.value = "";
+// 	boxesElements.innerHTML = "";
+// }
+// function getRandomHexColor() {
+// 	return `#${Math.floor(Math.random() * 16777215)
+// 		.toString(16)
+// 		.padStart(6, 0)}`;
+// }
